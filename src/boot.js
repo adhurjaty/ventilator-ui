e9aa177d07c49e0ea965cf277bbabe81ef83d834
Anil Dhurjaty <adhurjaty@gmail.com>
2020-04-03 15:48:44 -0700

    migrates app to newer react-native config

--- src/boot.js
+++ src/boot.js
@@ -1,16 +1,16 @@
-import React, { Component } from "react";
-import { Provider } from "react-redux";
-import configureStore from "./stores/configureStore";
-import { PersistGate } from "redux-persist/integration/react";
-import App from "./routers/App";
-import getTheme from "../native-base-theme/components";
-import material from "../native-base-theme/variables/material";
-import { StyleProvider } from "native-base";
+import React, {Component} from 'react';
+import {Provider} from 'react-redux';
+import configureStore from './stores/configureStore';
+import {PersistGate} from 'redux-persist/integration/react';
+import App from './routers/App';
+import getTheme from '../native-base-theme/components';
+import material from '../native-base-theme/variables/material';
+import {StyleProvider} from 'native-base';
 
 export default class Root extends Component {
   constructor(props) {
     super(props);
-    const { persistor, store } = configureStore();
+    const {persistor, store} = configureStore();
     this.persistor = persistor;
     this.store = store;
   }
@@ -19,7 +19,7 @@ export default class Root extends Component {
       <StyleProvider style={getTheme(material)}>
         <Provider store={this.store}>
           <PersistGate persistor={this.persistor}>
-            <App/>
+            <App />
           </PersistGate>
         </Provider>
       </StyleProvider>
@@ -27,23 +27,21 @@ export default class Root extends Component {
   }
 }
 
-global.XMLHttpRequest = global.originalXMLHttpRequest ?
-  global.originalXMLHttpRequest :
-  global.XMLHttpRequest;
-global.FormData = global.originalFormData ?
-  global.originalFormData :
-  global.FormData;
+global.XMLHttpRequest = global.originalXMLHttpRequest
+  ? global.originalXMLHttpRequest
+  : global.XMLHttpRequest;
+global.FormData = global.originalFormData
+  ? global.originalFormData
+  : global.FormData;
 
 fetch; // Ensure to get the lazy property
 
-if (window.__FETCH_SUPPORT__) { // it's RNDebugger only to have
+if (window.__FETCH_SUPPORT__) {
+  // it's RNDebugger only to have
   window.__FETCH_SUPPORT__.blob = false;
 } else {
-  global.Blob = global.originalBlob ?
-    global.originalBlob :
-    global.Blob;
-  global.FileReader = global.originalFileReader ?
-    global.originalFileReader :
-    global.FileReader;
+  global.Blob = global.originalBlob ? global.originalBlob : global.Blob;
+  global.FileReader = global.originalFileReader
+    ? global.originalFileReader
+    : global.FileReader;
 }
-
